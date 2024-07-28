fe43fea8f1fbccd574702c28892aa9d6ff3b1780
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2024-07-27 20:49:25 -0700

    e

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,35 +1,7 @@
 const routes:any = [
     {
-        id: "Application Advising",
-        route: "/application",
-        routePath: [
-            {
-                path: "/shop/mac",
-                route: "mac"
-            },
-            {
-                path: "/shop/ipad",
-                route: "ipad"
-            },
-            {
-                path: "/shop/iphone",
-                route: "iphone"
-            }
-        ],
-        paths: [
-            "/shop/mac",
-            '/shop/ipad',
-            '/shop/iphone',
-        ],
-        routeNames: [
-            "Mac",
-            "Ipad",
-            "Iphone"
-        ]
-    },
-    {
         id: "Major Advising",
-        route: "/mac",
+        route: "/",
         paths: [
             "/shop/mac",
             '/shop/ipad',
@@ -41,33 +13,6 @@ const routes:any = [
             "Iphone"
         ],
         routePath: [
-            {
-                path: "/shop/mac",
-                route: "mac"
-            },
-            {
-                path: "/shop/ipad",
-                route: "ipad"
-            },
-            {
-                path: "/shop/iphone",
-                route: "iphone"
-            }
-        ],
-    },
-    {
-        id: "Additional Resources",
-        route: "/resource",
-        paths: [
-            "/shop/mac",
-            '/shop/ipad',
-            '/shop/iphone',
-        ],
-        routeNames: [
-            "Mac",
-            "Ipad",
-            "Iphone"
-        ],        routePath: [
             {
                 path: "/shop/mac",
                 route: "mac"
