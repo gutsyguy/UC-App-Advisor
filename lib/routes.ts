7a53c4f6ecd77c2120dc8dee95202b2ed00820ce
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2024-07-27 20:51:48 -0700

    e

--- lib/routes.ts
+++ lib/routes.ts
@@ -27,6 +27,34 @@ const routes:any = [
             }
         ],
     },
+    {
+        id: "Application Advice",
+        route: "/application",
+        paths: [
+            "/shop/mac",
+            '/shop/ipad',
+            '/shop/iphone',
+        ],
+        routeNames: [
+            "Mac",
+            "Ipad",
+            "Iphone"
+        ],
+        routePath: [
+            {
+                path: "/shop/mac",
+                route: "mac"
+            },
+            {
+                path: "/shop/ipad",
+                route: "ipad"
+            },
+            {
+                path: "/shop/iphone",
+                route: "iphone"
+            }
+        ],
+    },
 ]
 
 export default routes
