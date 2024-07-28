587ea64e9104e87fec49b63c8153365206b4de34
Johnny Zheng <johnnyz.0527@gmail.com>
2024-07-27 20:39:34 -0700

    personal anecdote

--- app/resource/page.tsx
+++ app/resource/page.tsx
@@ -4,6 +4,40 @@ const resource = () => {
   return (
     <div className="min-h-screen bg-[#0080ff] text-white">
       <h1>Fire</h1>
+      <h2>Personal Anecdote from Johnny Zheng: </h2>
+      <p>
+
+      Hi! I am Johnny, an incoming freshman studying CS at UCLA. I believe that for me, there were a few big factors that contributed to my acceptance to UCLA. 
+
+The first of which was GPA. I took 15 AP classes in high school and passed all of them, with most scores being 4s and 5s. This ended up pushing my weighted GPA extremely high, breaking me into the top 5% of my graduating class. With 400 students in my graduating class, this meant that I was among the top 20 students. 
+
+If there is only one thing you can focus on for your application, it is absolutely your GPA. The UC Admissions does not look at standardized test scores, which limits its ability to detect high-achieving students in the classroom. The GPA is the only metric that tells them how good of a student you are. 
+
+For more evidence, I spoke with the other 19 students, and the large majority of them got into UC Berkeley, LA, SD, among other top 20 universities. 
+
+So, to summarize, try to be the best student you can be during high school. I understand that the area you are in and the school you go to limits the number of honors or AP classes you are allowed to take, which is why you just should aim to excel in academics in your own high school’s context. You are competing with students in your school, not others. It’s why colleges recalculate your GPA, they consider your profile in the context of your high school. 
+
+
+Secondly, you also have to excel outside of the classroom through extracurriculars. 
+
+Your extracurriculars are important because they will let the UC know about your character besides just studying in school. What do you do for fun? Are you interested in potentially engineering the next big product that will change the world? Showcase that to the admissions officers through your activities. 
+
+The UC admissions officers want to see that you have the potential to do great things in the future; accepting you is them believing in you and your potential. 
+
+In addition, your essays are another part of your UC application (that is super important). Here, the UC admissions gives you a chance to elaborate on your extracurriculars. Imagine what you would write for your essays if you hadn’t done any extracurriculars. 
+
+Here is a general guideline that I followed in high school when I picked what ECs to do: 
+Do something that aligns with your major
+Try to get leadership activities in school (clubs, programs, etc)
+Volunteering (help out your local church for example)
+Try to continue what you started in 9th grade and show dedication through length of participation
+
+Here is what you probably shouldn't do: 
+Start a new NPO in your junior year (its an obvious attempt of beefing up your app right before senior year)
+Passion is not faked, AOs can tell if you are being genuine
+Paying thousands for a summer program (free opportunities are just as good, if not better)
+
+      </p>
     </div>
   );
 };
